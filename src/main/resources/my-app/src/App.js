9f79f5070e3103a3ac5d4ce93ea31abf66f8cc9e
Chrisctr <credfern96@gmail.com>
2020-07-31 21:43:22 +0100

    Made changes to folder structure

--- src/main/resources/my-app/src/App.js
+++ src/main/resources/my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Wiki.js';
 
 
 function App() {
@@ -20,18 +21,17 @@ function App() {
           Learn React
         </a>
       </header>
-      <nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style={{width:250 + 'px'}} id="mySidebar">
-        <div class="w3-container w3-display-container w3-padding-16">
-          <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
-          <h3 class="w3-wide"><b>LOGO</b></h3>
+      <nav className="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style={{width:250 + 'px'}} id="mySidebar">
+        <div className="w3-container w3-display-container w3-padding-16">
+          <i onClick="w3_close()" className="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
+          <h3 className="w3-wide"><b>LOGO</b></h3>
         </div>
 
+        <div className="shopping-list"></div>
 
-        <div class="shopping-list"></div>
-
-        <a href="#footer" class="w3-bar-item w3-button w3-padding">Contact</a> 
-        <a href="javascript:void(0)" class="w3-bar-item w3-button w3-padding" onclick="document.getElementById('newsletter').style.display='block'">Newsletter</a> 
-        <a href="#footer"  class="w3-bar-item w3-button w3-padding">Subscribe</a>
+        <a href="#footer" className="w3-bar-item w3-button w3-padding">Contact</a> 
+        <a href="javascript:void(0)" className="w3-bar-item w3-button w3-padding" onClick="document.getElementById('newsletter').style.display='block'">Newsletter</a> 
+        <a href="#footer"  className="w3-bar-item w3-button w3-padding">Subscribe</a>
       </nav>
     </div>
   );
