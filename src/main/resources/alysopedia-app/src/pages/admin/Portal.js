6c9951efe021af6c73d920f4a592f7b9077b5532
Chrisctr <credfern96@gmail.com>
2020-08-13 15:54:05 +0100

    Finally got axios post for wiki entries working

--- src/main/resources/alysopedia-app/src/pages/admin/Portal.js
+++ src/main/resources/alysopedia-app/src/pages/admin/Portal.js
@@ -2,36 +2,39 @@ import React from 'react';
 import axios from 'axios';
 
 export default class portal extends React.Component {
-
-    state = {
-        title: '',
-        category: '',
-        author: 'Chris',
-        body: ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            category: '',
+            author: 'Chris',
+            body: ''
+        }
     }
 
-    handleChange = event => {
+    handleTitleChange = (event) => {       
         this.setState({ 
             title: event.target.value,
-            category: event.target.value,
-            author: event.target.value,
-            body: event.target.value 
         });
     }
 
-    handleSubmit = event => {
+    handleCategoryChange = (event) => {       
+        this.setState({ 
+            category: event.target.value,
+        });
+    }
+
+    handleBodyChange = (event) => {       
+        this.setState({ 
+            body: event.target.value,
+        });
+    }
+
+    handleSubmit = (event) => {
         event.preventDefault();
-    
-        const wiki = {
-            title: this.state.title,
-            category: this.state.category,
-            author: this.state.author,
-            body: this.state.body 
-        };
-    
-        axios.post(`http://localhost:8181/addWiki`, { wiki })
+        const { title, category, author, body } = this.state;
+        axios.post(`http://localhost:8181/addWiki`, { title, category, author, body })
           .then(res => {
-            console.log(res);
             console.log(res.data);
           })
       }
@@ -56,15 +59,15 @@ export default class portal extends React.Component {
                     <form onSubmit={this.handleSubmit}>
                         <section>
                             <label for="wikiName">Title<br></br></label>
-                            <input type="text" id="wikiName" name="title" placeholder="Put title here..." onChange={this.handleChange}></input>
+                            <input type="text" id="wikiName" name="title" placeholder="Put title here..." value={this.state.title} onChange={this.handleTitleChange}></input>
                         </section>
                         <section>
                             <label for="wikiCategory">Category<br></br></label>
-                            <input type="text" id="wikiCategory" name="category" placeholder="Put category here..." onChange={this.handleChange}></input>
+                            <input type="text" id="wikiCategory" name="category" placeholder="Put category here..." value={this.state.category} onChange={this.handleCategoryChange}></input>
                         </section>
                         <section>
                             <label for="wikiBody">Body<br></br></label>
-                            <input type="text" id="wikiBody" name="body" placeholder="Put body here..." className="body-input" onChange={this.handleChange}></input>
+                            <input type="text" id="wikiBody" name="body" placeholder="Put body here..." value={this.state.body} className="body-input" onChange={this.handleBodyChange}></input>
                         </section>
                         <button type="submit">Submit</button>
                     </form>
