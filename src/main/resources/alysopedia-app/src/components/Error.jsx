0e57b94e3fd5864acf83320f7070e0ab79f3b0e1
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-08-19 11:25:05 +0100

    replaced Error.js with components

---
+++ src/main/resources/alysopedia-app/src/components/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+const Error = () => {
+    return (
+       <div>
+          <p>Error : Something has gone wrong! :(</p>
+       </div>
+    );
+}
+ 
+export default Error;
