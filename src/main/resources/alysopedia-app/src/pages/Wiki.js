bc90f57f5f21c740801caca39c1b962260caaf06
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-08-20 15:36:22 +0100

    added states to wiki.js so it is displayed from DB

--- src/main/resources/alysopedia-app/src/pages/Wiki.js
+++ src/main/resources/alysopedia-app/src/pages/Wiki.js
@@ -1,47 +1,71 @@
 
-import React from 'react';
-import logo from '../images/logo.svg';
+import React, {useState} from 'react';
 import './Wiki.js';
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
 import axios from 'axios';
 import Article from '../components/Article';
+import SideSummary from '../components/wiki/Side-Summary';
+import logo from '../images/logo.svg';
 
-let configGet = {
+const Wiki = () => {
+
+    //states
+    const[data, setData] = useState([]);
+    const[title, setTitle] = useState(`Title`);
+    const[catagory, setCatagory] = useState(`Catagory`);
+    const[body, setBody] = useState(`Body`);
+
+    //page to retrieve
+    let reqUrl = `http://localhost:8181/readWikiByName/Test`;
+
+// request for data
+    let configGet = {
     headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
     responseType: 'json'
-};
+    };
 
 const searchWiki = () => {
-    let wikiTitle = document.getElementById('wikiName')
     
-    axios.get(`http://localhost:8181/readWikiByName/Test`, configGet)
+    axios.get(reqUrl, configGet)
         .then(function (response) {
-            let titleArr = response.data[0].title;
+            let data = response.data;
+            setData(data);
+            
+            setTitle(response.data[0].title);
+            setCatagory(response.data[0].category);
+            setBody(response.data[0].body);
 
-            document.querySelector('#wikiName').innerHTML = response.data[0].title;
-            document.querySelector('#wikiCategory').innerHTML = response.data[0].category;
-            document.querySelector('#wikiBody').innerHTML = response.data[0].body;
-            console.log(response.data);
-            console.log(titleArr);
-
+            console.log(data);
+            console.log(title);
+            console.log(catagory);
+            console.log(body);
         })
         .catch(function (error) {
             console.log(error);
         });
 }
+
 window.addEventListener("load", searchWiki);
 
-const Wiki = () => {
+// wiki page display - as long as data returned is empty page will be 'loading'
   return (
     <div className="Wiki">
         <Navigation />
 
         <div style={{marginLeft:250 + 'px'}}>
-            <Article 
-            title="WikiName"
-            subtitleText="Catagory: ${}"
-            body="wiki bodyyyyyyyyyyyyy"/>
+            {
+                Array.isArray(data) && data.length > 0 ?
+                <Article 
+                    title={title}
+                    subtitleText={catagory}
+                    body={body}
+                />
+            : <div>
+                <h1>Wiki data is loading...</h1> <img src={logo} className="App-logo" alt="logo" />
+              </div>
+            }
+            <SideSummary />
             <Footer />
         </div>
         
