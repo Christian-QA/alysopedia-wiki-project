c5dd9e141df69c4a6228888472fb189a1f41aa2b
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-08-20 13:52:05 +0100

    modified footer shrank (c) line

--- src/main/resources/alysopedia-app/src/components/Footer.jsx
+++ src/main/resources/alysopedia-app/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
       <div className="App-footer">
         <p>This is the placeholder Asylopedia footer</p>
-        <p>Copyright 2020 &copy;</p>
+        <p><small>Copyright 2020 &copy;</small></p>
       </div>
     );
 }
