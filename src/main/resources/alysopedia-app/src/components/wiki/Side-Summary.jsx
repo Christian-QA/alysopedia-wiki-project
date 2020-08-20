408b571fcfe984c88968d327a805e15a87265043
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-08-20 15:33:49 +0100

    added to side summary component

---
+++ src/main/resources/alysopedia-app/src/components/wiki/Side-Summary.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../../images/logo.svg';
+
+const SideSummary = () => {
+    return (
+      <div className="App" style={{flexDirection: "row-reverse"}}>
+          <p>Title</p>
+          {/* <img src={logo}  alt="logo" /> */}
+            <table>
+                <tbody>
+                    <tr><th>List item: </th><td>One Item</td></tr>
+                    <tr><th>List item: </th><td>Two Item</td></tr>
+                    <tr><th>List item: </th><td>Three Item</td></tr>
+                    <tr><th>List item: </th><td>Four Item</td></tr>
+                </tbody>
+            </table>
+      </div>
+    );
+}
+
+export default SideSummary;
