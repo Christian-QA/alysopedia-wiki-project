08a79009c0c0c6dfac72e4e737c4404087554d7d
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-08-20 13:41:32 +0100

    stylised footer

--- src/main/resources/alysopedia-app/src/components/Footer.jsx
+++ src/main/resources/alysopedia-app/src/components/Footer.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 const Footer = () => {
     return (
-    <>
-      <div className="footer">
+      <div className="App-footer">
         <p>This is the placeholder Asylopedia footer</p>
-        <p>Copyright 2020</p>
+        <p>Copyright 2020 &copy;</p>
       </div>
-   </>
     );
 }
 
