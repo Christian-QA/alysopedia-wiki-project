978cce2c3ca0c13f5b1b4a01abc0af2ecf423fdd
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-08-21 16:28:17 +0100

    modified search bar to inline style

--- src/main/resources/alysopedia-app/src/components/WikiSearch.jsx
+++ src/main/resources/alysopedia-app/src/components/WikiSearch.jsx
@@ -5,13 +5,12 @@ const WikiSearch = ({wikiTitle, onWikiTitleChange, onSubmit}) => {
     return (
        <> 
       <div className="App" style={{flexDirection: "row-reserve"}}>
-          <p>Search</p>
           <form onSubmit={onSubmit}>
             <section>
-                    <label for="wikiName">By Title<br></br></label>
+                    <label for="wikiName">Search By Title: </label>
                     <input type="text" id="wikiName" name="title" placeholder="Looking for title..." value={wikiTitle} onChange={onWikiTitleChange}></input>
-            </section>
             <button type="submit">Submit</button>
+            </section>
           </form>
       </div>
       </>
