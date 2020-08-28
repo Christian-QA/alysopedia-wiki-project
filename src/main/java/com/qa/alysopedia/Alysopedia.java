619554ce9782adbae9f106b739c8debd0938dc72
Chrisctr <credfern96@gmail.com>
2020-08-28 22:18:27 +0100

    Fixes to the security system implemented. Security system currently not linked to front-end as new architecture needs to be considered

--- src/main/java/com/qa/alysopedia/Alysopedia.java
+++ src/main/java/com/qa/alysopedia/Alysopedia.java
@@ -2,10 +2,12 @@ package com.qa.alysopedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com.qa.alysopedia"})
+@EnableCaching(proxyTargetClass = true)
 public class Alysopedia {
 	public static void main(String[] args) {
 		SpringApplication.run(Alysopedia.class, args);
