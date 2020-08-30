7690d673a0a720997c4f42e8d0cafbe321b5e76d
Chrisctr <credfern96@gmail.com>
2020-08-30 21:53:39 +0100

    Got early version of sublist population working, just need to split comma seperated values

--- src/main/resources/alysopedia-app/src/components/SideList.jsx
+++ src/main/resources/alysopedia-app/src/components/SideList.jsx
@@ -2,112 +2,60 @@ import React from 'react';
 import Collapsible from 'react-collapsible';
 
 let data = [{
-    "title": "Nat 1",
     "category": "Cat 1",
-    "subcategory": "Sub 1",
-    "author": "Chris",
-    "body": "fghdhyjd"
-},
-{    
-    "title": "Nat 1",
-    "category": "Cat 1, Sub 1",
-    "subcategory": "Sub 1",
-    "author": "Chris",
-    "body": "fghdhyjd"
-},
-{    
-    "title": "Nat 1",
-    "category": "Cat 1, Sub 2",
-    "subcategory": "Sub 2",
-    "author": "Chris",
-    "body": "fghdhyjd"
+    "subcategory": "Sub 1, Sub 2, Sub 3"
 },
 {
-    "title": "Nat 2",
     "category": "Cat 2",
-    "subcategory": "Sub 1",
-    "author": "Chris",
-    "body": "dght"
-},
-{    
-    "title": "Nat 1",
-    "category": "Cat 2",
-    "subcategory": "Sub 2",
-    "author": "Chris",
-    "body": "fghdhyjd"
+    "subcategory": "Sub 1, Sub 4, Sub 5"
 },
 {
-    "title": "Nat 3",
     "category": "Cat 3",
-    "subcategory": "Sub 2",
-    "author": "Chris",
-    "body": "waesfserg"
-},
-{
-    "title": "Nat 3",
-    "category": "Cat 3",
-    "subcategory": "Sub 3",
-    "author": "Chris",
-    "body": "waesfserg"
-},
-{
-    "title": "Nat 4",
-    "category": "Cat 3",
-    "subcategory": "Sub 2",
-    "author": "Chris",
-    "body": "waesfserg"
-},
-{
-    "title": "Nat 4",
-    "category": "Cat 4, Sub 4",
-    "subcategory": "Sub 4",
-    "author": "Chris",
-    "body": "waesfserg"
-}];
-
-
-function removeDuplicates(array) {
-    array = array.map(JSON.stringify).reverse() // convert to JSON string the array content, then reverse it (to check from end to begining)
-    .filter(function(item, index, array){ return array.indexOf(item, index + 1) === -1; }) // check if there is any occurence of the item in whole array
-    .reverse().map(JSON.parse) // revert it to original state
-    return array;
+    "subcategory": "Sub 1, Sub 4, Sub 5, Sub 6, Sub 7, Sub 99"
 }
+];
+
 
 const SideList = () =>  {
-    let indexSplit = [];
+    let indexCat = [];
+    let indexSub = [];
+    let listItems;
+
     for (let i = 0; i < data.length; i++) {
-        indexSplit[i] = data[i].category.split(',')
-        
-        console.log(indexSplit[i][0]);
-        console.log(indexSplit[i][1]);
+
+        indexCat[i] = data[i].category;
+
+        indexSub[i] = data[i].subcategory;
+
+        listItems = indexCat.map((indexCat, i) =>
+        <>
+            <Collapsible trigger={indexCat} className="Category-main" >
+                <Collapsible trigger={indexSub[i]} className="Category-sub" >
+                    <p>This is the collapsible content.</p>
+                </Collapsible>
+            </Collapsible>
+        </>
+        );
+        console.log(listItems[i])
 
     }
-    console.log(indexSplit)
-
-    let indexSplitFinal = removeDuplicates(indexSplit)
-
-    console.log(indexSplitFinal)
-
     
-    const subListItems = indexSplitFinal.map((indexSplitFinal) =>
-        <>
-            <Collapsible trigger={indexSplitFinal[1]} className="Category-sub" >
-                <p>This is the collapsible content.</p>
-            </Collapsible>
-        </>
-    );
 
-    const listItems = indexSplitFinal.map((indexSplitFinal) =>
-        <>
-            <Collapsible trigger={indexSplitFinal[0]} className="Category-main" >
-                    { subListItems }
-            </Collapsible>
-        </>
-    );
+    console.log(listItems[0])
+    console.log(listItems[1])
+
+    // const subListItems = indexSub.map((indexSub) =>
+    //     <>
+    //         <Collapsible trigger={indexSub} className="Category-sub" >
+    //             <p>This is the collapsible content.</p>
+    //         </Collapsible>
+    //     </>
+    // );
 
 
-    
-    console.log(subListItems.toString())
+
+
+    console.log(listItems)
     return (        
         <div>{listItems}</div>
     )
