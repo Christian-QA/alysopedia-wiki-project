b85c666fa66686c729b51f605766a73703afc255
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2020-08-31 16:35:45 +0100

    adding WikiSearch in Navigation - commented as breaking rule of Hooks

--- src/main/resources/alysopedia-app/src/components/Navigation.jsx
+++ src/main/resources/alysopedia-app/src/components/Navigation.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
+// import React, {useState} from 'react';
 import { NavLink } from 'react-router-dom';
-import WikiSearch from './WikiSearch';
+//import WikiSearch from './WikiSearch';
+//import axios from 'axios';
 
+//    // For WikiSearch.jsx
+//    const reqUrl = `http://localhost:8181/readWikiByCategory/`;
+//    //pages to retrieve - currently default to Test
+//    let wikiDefaultSearch = `Test`;
+
+//    // request for data
+//    let configGet = {
+//    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+//    responseType: 'json'
+//    };
+
+//    const[wikiSearch, setWikiSearch] = useState(wikiDefaultSearch);
+
+//    const handleWikiSearchChange = (event) => {       
+//    setWikiSearch(event.target.value);
+//    console.log(wikiSearch);
+//    }
+
+//    const handleSearchSubmit = (event) => {
+//    event.preventDefault();
+//    searchWiki();
+//    }
+
+//    const searchWiki = () => {
+//       axios.get(reqUrl + wikiSearch, configGet)
+//           .then(response => {
+//              // redirect to /Wiki or /WikiLanding
+//              // send data for display
+//               let data = response.data;
+//             //   setData(data);
+              
+//             //   setTitle(response.data[0].title);
+//             //   setCatagory(response.data[0].category);
+//             //   setBody(response.data[0].body);
+//             //   //set to response once feature added
+//             //   setWiki({
+//             //       contents: false,
+//             //       table: false
+//             //       });
+
+//               console.log(data);
+//           })
+//           .catch(function (error) {
+//               console.log(error);
+//           });
+//   }
  
 const Navigation = () => {
     return (
@@ -11,6 +59,11 @@ const Navigation = () => {
             <div className="w3-container w3-display-container w3-padding-16">
                <NavLink className="w3-wide" to="/"><b>Logo</b></NavLink>
             </div>
+            {/* <WikiSearch
+            label="Search by Category: "
+            wikiSearch={wikiSearch}
+            onSearchChange={handleWikiSearchChange}
+            onSubmit={handleSearchSubmit}/> */}
             <NavLink className="w3-bar-item w3-button w3-padding" to="/Wiki">Wiki</NavLink>
             <NavLink className="w3-bar-item w3-button w3-padding" to="/About">About</NavLink>
             <NavLink className="w3-bar-item w3-button w3-padding" to="/Admin">Portal</NavLink>
